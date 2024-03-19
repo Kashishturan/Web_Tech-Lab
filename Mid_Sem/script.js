6ede210ad4dc7bfefd202d199ae3df9aa721b718
Kashishturan <kashishturan884@gmail.com>
2024-03-19 10:33:20 +0530

    Web Tech lab commit

---
+++ Mid_Sem/script.js
@@ -0,0 +1,30 @@
+
+// // /*let newBtn=document.createElement("Button");
+// // newBtn.innerText="Click me";
+// // newBtn.style.color="white";
+// // newBtn.style.backgroundColor="red";
+// // document.querySelector("body").prepend(newBtn); */
+// // let k = document.querySelector("button");
+// // k.onclick = ()=>{
+// // console.log("HEYY");
+// // alert("HII");
+// // // }
+// // let x = number(prompt("Enter value:", x));
+// // // let y = number(prompt("Enter value:", y));
+// // let a;
+// // let b;
+// function mum(x,y){
+//     let  summ=x+y;
+//     return summ;
+
+// }
+
+// let x;
+// (prompt("Enter value:", x));
+// let y;
+// (prompt("Enter value:", y));
+// window.alert(mum(x,y));
+let div = document.getElementsByClassName(a);
+div.onclick = () => {
+    console.log('hi')
+}
